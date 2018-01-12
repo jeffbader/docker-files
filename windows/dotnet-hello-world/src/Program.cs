e4110b27f9fef56b671ba8f08b33a07f6467e5a0
Jeff Bader <redabaj@gmail.com>
2018-01-11 19:16:13 -0800

    Initial commit

---
+++ windows/dotnet-hello-world/src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld.NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
